eb590a2a20a1333225d5d0429a8301bb2f256780
Michaela Wagner <mickywagner@gmail.com>
2019-08-20 18:57:28 -0600

    Add books to library through modal form

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function Book(title, author, pages, read) {
     this.read = read
     this.info = function() {
         if(this.read == false) {
-            return `${this.title} by ${this.author}, ${this.pages}, read`
-        } else {
             return `${this.title} by ${this.author}, ${this.pages}, not read`
+        } else {
+            return `${this.title} by ${this.author}, ${this.pages}, read`
         }
     }
 }
@@ -17,3 +17,43 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
+
+let bookModal = document.querySelector(".addBookModal")
+const newBook = document.querySelector("#newBook")
+const closeBtn = document.querySelector(".close-button")
+
+function toggleModal() {
+    bookModal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === bookModal) {
+        toggleModal();
+    }
+}
+
+newBook.addEventListener('click', toggleModal)
+window.addEventListener('click', windowOnClick)
+closeBtn.addEventListener('click', toggleModal)
+
+let title = document.querySelector("#title")
+let author = document.querySelector("#author")
+let pages = document.querySelector("#pages")
+let checkBox = document.querySelector("#read")
+
+let submitBtn = document.querySelector("input[type=submit]")
+
+submitBtn.addEventListener('click', function() {
+    let bTitle = title.value
+    let bAuthor = author.value
+    let bPages = pages.value
+    if(checkBox.checked == true) {
+        bRead = true
+    } else {
+        bRead = false
+    }
+    let entry = new Book(bTitle, bAuthor, bPages, bRead)
+    addBookToLibrary(entry)
+    toggleModal()
+    console.log(myLibrary)
+})
